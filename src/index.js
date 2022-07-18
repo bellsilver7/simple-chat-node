124ba0508a7236ebf286f0255b6de80beff2184e
bellsilver7 <bellsilver7@naver.com>
2022-07-18 17:35:14 +0900

    Broadcasting

--- src/index.js
+++ src/index.js
@@ -12,14 +12,21 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("a user connected");
 
+  socket.broadcast.emit("hi");
+
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    io.emit("chat message", msg);
   });
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 });
+
+io.emit("some event", {
+  someProperty: "some value",
+  otherProperty: "other value",
+}); // This will emit the event to all connected sockets
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
