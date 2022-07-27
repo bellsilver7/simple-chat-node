3840b755f3ad3ea5b28fa5b3a492fa3eaf5e49f8
bellsilver7 <bellsilver7@naver.com>
2022-07-27 14:12:22 +0900

    Add private message and Share

--- src/index.js
+++ src/index.js
@@ -9,52 +9,42 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-let rooms = {
-  room_1: { title: "채팅방1", users: [] },
-  room_2: { title: "채팅방2", users: [] },
-  room_3: { title: "채팅방3", users: [] },
-};
 let users = [];
 io.on("connection", (socket) => {
-  let currentRoomId = "";
-  let roomIo = null;
+  let number = users.length + 1;
+  socket.nickname = `Guest${number}`;
+  users.push(socket.nickname);
+  console.log(`${socket.nickname} connected / ${socket.id}`);
+  io.emit("chat message", `${socket.nickname} connected`);
+  io.emit("online", users);
 
-  socket.on("new user", (nickname) => {
-    socket.nickname = nickname;
-    io.emit("room list", rooms);
+  socket.on("disconnect", () => {
+    console.log(`${socket.nickname} disconnected`);
+    io.emit("chat message", `${socket.nickname} disconnected`);
+    users.pop(socket.nickname);
+    io.emit("offline", socket.nickname);
   });
 
-  socket.on("join room", (roomId) => {
-    // roomId = "room_" + socket.id;
-    roomIo = io.to(roomId);
-    socket.join(roomId);
-    rooms[roomId].users.push(socket.nickname);
-    roomIo.emit("online", rooms[roomId].users);
-    roomIo.emit("chat message", `${socket.nickname}이(가) 들어왔어요.`);
-  });
-
-  socket.on("is typing", (nickname) => {
-    socket.join(currentRoomId).broadcast.emit("is typing", nickname);
+  socket.on("typing", (bool) => {
+    let user = bool ? socket.nickname : "";
+    io.emit("typing", user);
   });
 
   socket.on("chat message", (msg) => {
-    const message = msg.trim();
-
-    if (message === socket.chatMessage) return;
-    socket.chatMessage = message;
-
-    const date = new Date();
-    const currentTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-    roomIo.emit(
-      "chat message",
-      `${socket.nickname}: ${message} (${currentTime})`
-    );
+    if (msg !== socket.prevMessage) {
+      socket.prevMessage = msg;
+      console.log(`${socket.nickname}: ${msg}`);
+      io.emit("chat message", `${socket.nickname}: ${msg}`);
+    }
   });
 
-  socket.on("disconnect", () => {
-    users.splice(socket.nickname);
-    roomIo.emit("offline", socket.nickname);
-    roomIo.emit("chat message", `${socket.nickname}이(가) 나갔어요.`);
+  socket.on("private message", (msg) => {
+    let message = msg.split("#");
+    console.log(`private ${socket.nickname}: ${message[1]}`);
+    io.to(socket.id).emit(
+      "private message",
+      `${socket.nickname}: ${message[1]}`
+    );
   });
 });
 
